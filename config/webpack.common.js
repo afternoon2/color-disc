26f5e083d2c7b3adc4e74310fc55a1e3c80d83c3
Jakub Antolak <jakub.antolak@envoydigital.com>
2019-05-30 17:59:06 +0200

    add configuration, server and blank src

---
+++ config/webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'color-disc.js',
+    library: 'ColorDisc',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[folder]-[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: loader => [
+                require('autoprefixer')({
+                  browsers: ['last 5 version', '> 1%']
+                }),
+                require('cssnano')()
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,22 @@
+  
+const path = require('path');
+const merge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge({
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../server'),
+    inline: true,
+    hot: true
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../dist/color-disc.js'),
+        to: path.resolve(__dirname, '../server/color-disc.js')
+      }
+    ])
+  ]
+}, common);
