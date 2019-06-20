5c405a1728145cb87ac1e83ed92d333a5e0a3b14
afternoon2 <poprostuantolak@gmail.com>
2019-06-21 01:09:24 +0200

    Revert "Merge branch 'master' into canvas-only"
    This reverts commit 1452ca56f044a966812bd8c44002d3fb5b6f4e86, reversing
    changes made to cad76080b8456012209ade9415d493af8e896e6b.

--- src/color-disc/ColorDisc.js
+++ src/color-disc/ColorDisc.js
@@ -34,18 +34,18 @@ export default class ColorDisc extends HTMLElement {
     this.__colorObserver.disconnect();
   }
 
-
-  _render() {
+  __render() {
     const { locals } = styles;
+    this.setAttribute('color', 'hsl(0, 100%, 50%)');
     this.padding = 20;
-    this._size = (parseInt(this.getAttribute('size'), 10) || 390) - this.padding;
+    this._size = (parseInt(this.getAttribute('size'), 10) || 390);
     this.shadowRoot.innerHTML = `
       <style>
         ${styles.toString().replace(/\n|\t/g, '')}
       </style>
       <div
         class="${locals.colorDisc}"
-        style="width: ${this._size}px; height: ${this._size}px; left: calc(50% - ${this._size}px / 2)"
+        style="width: ${this._size}px; height: ${this._size}px;"
       >
         <color-stage
           size="${this._size}"
