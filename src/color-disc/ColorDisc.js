84b6b41a79fad66001de063f302b96e464f75223
afternoon2 <poprostuantolak@gmail.com>
2019-06-22 14:05:32 +0200

    add format change handler

--- src/color-disc/ColorDisc.js
+++ src/color-disc/ColorDisc.js
@@ -15,11 +15,14 @@ export default class ColorDisc extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue !== newValue) {
+    if (oldValue !== newValue && this.shadowRoot.childNodes.length > 0) {
       switch (name) {
         case 'size':
           this.__render();
           this.__observeStage();
+          break;
+        case 'format':
+          this.__onColorFormatChange(newValue, this.shadowRoot.querySelector('color-stage'));
           break;
       }
     }
@@ -36,7 +39,8 @@ export default class ColorDisc extends HTMLElement {
 
   __render() {
     const { locals } = styles;
-    this.setAttribute('color', 'hsl(0, 100%, 50%)');
+    const formats = this.__getAllFormats(0, 100, 50);
+    this.setAttribute('color', formats[this.getAttribute('format')]);
     this.__padding = 20;
     this.__size = (parseInt(this.getAttribute('size'), 10) || 390);
     this.shadowRoot.innerHTML = `
@@ -56,32 +60,34 @@ export default class ColorDisc extends HTMLElement {
   }
 
   __observeStage() {
+    const self = this;
     const target = this.shadowRoot.querySelector('color-stage');
     const config = { attributes: true };
     const callback = (mutationsList) => {
-      mutationsList.forEach((mutation) => {
-        const h = mutation.target.getAttribute('h');
-        const s = mutation.target.getAttribute('s');
-        const l = mutation.target.getAttribute('l');
-        switch (this.getAttribute('format')) {
-          case 'hsl':
-            this.setAttribute('color', `hsl(${h}, ${s}%, ${l}%)`);
-            break;
-          case 'rgb':
-            this.setAttribute('color', math.getRgb(h, s, l));
-            break;
-          case 'hex':
-            this.setAttribute('color', math.getHex(
-              ...Object.values(math.getRgb(h, s, l, true)),
-            ));
-            break;
-          default:
-            this.setAttribute('color', `hsl(${h}, ${s}%, ${l}%)`);
-            break;
-        }
-      });
+      mutationsList.forEach(mutation => this.__onColorFormatChange(
+        self.getAttribute('format'),
+        mutation.target,
+      ));
     };
     this.__colorObserver = new MutationObserver(callback);
     this.__colorObserver.observe(target, config);
+  }
+
+  __onColorFormatChange(format, stage) {
+    const h = parseInt(stage.getAttribute('h'), 10);
+    const s = parseInt(stage.getAttribute('s'), 10);
+    const l = parseInt(stage.getAttribute('l'), 10);
+    const formats = this.__getAllFormats(h, s, l);
+    this.setAttribute('color', formats[format]);
+  }
+
+  __getAllFormats(h, s, l) {
+    return {
+      hsl: `hsl(${h}, ${s}%, ${l}%)`,
+      rgb: math.getRgb(h, s, l),
+      hex: math.getHex(
+        ...Object.values(math.getRgb(h, s, l, true)),
+      ),
+    };
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import ColorDisc from './color-disc/ColorDisc';
 import ColorStage from './color-stage/ColorStage';
 
-(() => {
-  const elements = [
-    { tag: 'color-stage', node: ColorStage },
-    { tag: 'color-disc', node: ColorDisc },
-  ];
-  elements.forEach((element) => {
-    if (!customElements.get(element.tag)) {
-      customElements.define(element.tag, element.node);
-    }
-  });
-})();
+const elements = [
+  { tag: 'color-stage', node: ColorStage },
+  { tag: 'color-disc', node: ColorDisc },
+];
+elements.forEach((element) => {
+  if (!customElements.get(element.tag)) {
+    customElements.define(element.tag, element.node);
+  }
+});
